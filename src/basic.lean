f9e58fb0d9ce014f4464218207a79e12223b7423
owen-fool <owenrmilner@gmail.com>
2021-06-28 20:33:08 +0100

    first commit

---
+++ src/basic.lean
@@ -0,0 +1,11 @@
+import tactic
+import topology.basic topology.path_connected
+
+open classical
+open_locale classical
+noncomputable theory
+
+universes u v w
+
+variables (X : Type u) [τ : topological_space X]
+
